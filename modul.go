bba50a8178409257e388acc6fe9c5992d0f5cba4
Muhammad Iqbal <iqbal251196@gmail.com>
2022-03-12 09:29:40 +0700

    kedua

--- modul.go
+++ modul.go
@@ -6,7 +6,7 @@ import "fmt"
 
 func Welcome(nama, jawaban string) {
 	fmt.Println("Selamat Datang di Lapak Kami, Apakah anda ingin berbelanja(Y/N)")
-        if jawaban== Y{
+        if jawaban== "Y"{
 	fmt.Println("Silahkan masukkan nama Anda")
 	fmt.Scanln(&nama)
 	fmt.Printf("Hai kak %q, silahkan untuk melihat lihat produk kami\n",nama)
@@ -16,7 +16,7 @@ func Welcome(nama, jawaban string) {
 }
 //function variadic
 func ItemPakaian(nama string, pilihanBaju... string){
-	var bajuAsstring = string.Join(pilihanBaju,",")
+	var bajuAsstring = strings.Join(pilihanBaju,",")
 	fmt.Printf("Hello Mr/Mrs %s\n", nama)
 	fmt.Printf("This is a selection of clothes in our shop %s\n", bajuAsstring)
 }
@@ -29,16 +29,16 @@ func ProfitSale(harga, hargaPokok float64) (float64, float64) {
 // anonymousstruct
 func Pembayaran() {
 	Bayar := struct {
-		Diskon int
-		Pay int
+		Diskon float64
+		Pay float64
 	}{
-		Diskon: 10/100 * harga,
-		Pay: harga,
+		Diskon: 20000,
+		Pay: 200000,
 	}
 	if Pembayaran.Pay > 100000{
 
-		fmt.Println("Selamat Anda Mendapat Potongan Harga", Pembayaran.Diskon, "Jumlah yang dibayarkan", (Pembayaran.Pay-Pembayaran.Diskon))
+		fmt.Println("Selamat Anda Mendapat Potongan Harga", Bayar.Diskon, "Jumlah yang dibayarkan", (Bayar.Pay-Bayar.Diskon))
 	} else {
-		fmt.Println("Maaf anda tidak mendapat ptongan harga, Jumlah yang dibayarkan", Pembayaran.Pay)
+		fmt.Println("Maaf anda tidak mendapat ptongan harga, Jumlah yang dibayarkan", Bayar.Pay)
 	}
 }
